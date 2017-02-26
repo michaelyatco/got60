5f98392b803f51b6b7707f8917ca94c206d5ccd7
Michael Yatco <mjyatco@gmail.com>
2017-02-26 17:00:24 -0500

    First commit with theme; questions added and trivia controller

--- app/controllers/trivias_controller.rb
+++ app/controllers/trivias_controller.rb
@@ -6,5 +6,14 @@ class TriviasController < ApplicationController
     render "index.html.erb"
   end
 
-    
+  def show
+   @trivia_question = TriviaQuestion.all.sample 
+   @trivia_answers = TriviaAnswer.where(trivia_question_id: @trivia_question.id)
+    if params[:answer] == true
+      score += 1
+      # flash message for correct answer
+    else
+      # flash for incorrect
+    end
+  end    
 end
