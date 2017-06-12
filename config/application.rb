adb4c2410691edc4105079ea326ade679f820e12
Michael Yatco <mjyatco@gmail.com>
2017-06-12 08:28:49 -0400

    Rspec Generator

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,14 @@
+config.generators do |g|
+  g.test_framework :rspec,
+    fixtures: true,
+    view_specs: false,
+    helper_specs: false,
+    routing_specs: false,
+    controller_specs: true,
+    respect_specs: false
+  g.fixture_replacement :factory_girl, dir: "spec/factories"
+end
+
 require_relative 'boot'
 
 require 'rails/all'
