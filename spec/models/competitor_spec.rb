8a71e9d4f95823c7aa6ddda6db7c0a2fa22be0cf
michaelyatco <mjyatco@gmail.com>
2017-06-12 23:03:09 -0400

    More Rspec tests

--- spec/models/competitor_spec.rb
+++ spec/models/competitor_spec.rb
@@ -13,4 +13,25 @@ describe Competitor do
   	competitor = Competitor.create(email: "Mike@Yatco.com", username: "Mike")
   	expect(competitor).not_to be_valid
   end
+  it "is invalid without a username" do
+	competitor = Competitor.new(username: nil)
+	competitor.valid?
+	expect(competitor.errors[:username]).to include("can't be blank")
+  end
+  it "is invalid with a duplicate email" do
+  	Competitor.create(
+  		username: "Mike", 
+  		email: "Mike@Yatco.com",
+  		password: "Mike"
+  		)
+  	
+  	competitor = Competitor.new(
+  		username: "Dani", 
+  		email: "Mike@Yatco.com"
+  		password: "Mike"
+  		)
+
+  	competitor.valid?
+    expect(competitor.errors[:email]).to include("has already been taken")
+   end
 end
